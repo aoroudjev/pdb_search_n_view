18d4e709cce39f2a81135be8abf1ef57e07dbdab
aoroudjev <Alexander_Oroudjev@waters.com>
2024-05-08 13:52:13 -0700

    Add multiple pages

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,22 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(hello_world()));
 
-    // run our app with hyper, listening globally on port 3000
+    let app = Router::new()
+    .route("/", get(""))
+    .route("/home", get(home()))
+    .route("/hello", get(hello_world()));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 fn hello_world() -> &'static str{
     "Hello, world!"
+}
+
+fn home() -> &'static str{
+    "Home"
 }
 
 
