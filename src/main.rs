ba25c74346ad3cd69e2dd0329ffa1845454cca26
Alexander Oroudjev <a.oroudjev@gmail.com>
2024-05-08 20:46:26 -0700

    Fix Search handling

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,65 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get};
+use axum::extract::Query;
+use axum::http::StatusCode;
+use serde::{Serialize, Deserialize};
+
+use urlencoding::encode;
+use reqwest;
+
+#[derive(Default, Serialize, Deserialize, Debug)]
+struct SearchResult {
+    //TODO: Fix structure of returns
+    pdb_id: String,
+    name: String,
+    description: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(search_handler()))
-        .route("/home", get(home()))
-        .route("/hello", get(hello_world()),
-        );
+    let app = Router::new().route("/search", get(search_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-fn hello_world() -> &'static str {
-    "Hello, world!"
+#[derive(Deserialize)]
+struct PdbId {
+    pdb_id: String,
 }
 
-fn home() -> &'static str {
-    "Home"
-}
+async fn search_handler(Query(params): Query<PdbId>) -> Result<Json<SearchResult>, StatusCode> {
+    let search_input = params.pdb_id;
+    let search_request = serde_json::json!({
+          "query": {
+            "type": "terminal",
+            "service": "full_text",
+            "parameters": {
+              "value": search_input
+            }
+          },
+          "return_type": "entry"
+        }).to_string();
+    dbg!(&search_request);
 
-fn search_handler() { }
 
+    let encoded_search_request = encode(&search_request);
+    let api_url = format!("https://search.rcsb.org/rcsbsearch/v2/query?json={}", encoded_search_request);
+    let client = reqwest::Client::new();
 
-#[cfg(test)]
-mod test {
-    use super::*;
+    let search_result_json = client.get(api_url)
+        .send()
+        .await
+        .map_err(|e| {
+            dbg!(e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?.text().await;
+    // .json::<SearchResult>()
+    // .await
+    // .map_err(|e| {
+    //     dbg!(e);
+    //     StatusCode::INTERNAL_SERVER_ERROR
+    // })?;
+    dbg!(&search_result_json);
 
-    #[test]
-    fn test_hello_world() {
-        assert_eq!(hello_world(), "Hello, world!");
-    }
+    Ok(Json(Default::default()))
 }
