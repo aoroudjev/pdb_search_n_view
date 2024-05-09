25adbbdffc978369b3d76ddb4dd22200ae0102b1
Alexander Oroudjev <alexander_oroudjev@waters.com>
2024-05-08 17:27:10 -0700

    Add handler examples

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-
     let app = Router::new()
-    .route("/", get(""))
-    .route("/home", get(home()))
-    .route("/hello", get(hello_world()));
+        .route("/", get(search_handler()))
+        .route("/home", get(home()))
+        .route("/hello", get(hello_world()),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-fn hello_world() -> &'static str{
+fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
-fn home() -> &'static str{
+fn home() -> &'static str {
     "Home"
 }
+
+fn search_handler() { }
 
 
 #[cfg(test)]
