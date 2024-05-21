865cd57eef08d77e37fec78edd5ef0277532b190
Alexander Oroudjev <alexander_oroudjev@waters.com>
2024-05-21 15:42:53 -0700

    Begin Uniprot API attempt

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,54 @@
-use std::collections::HashMap;
-use std::ffi::CString;
 use axum::{Json, Router, routing::get};
 use axum::extract::Query;
 use axum::http::StatusCode;
-use axum::body::Bytes;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-use urlencoding::encode;
 use reqwest;
-use reqwest::Response;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct SearchResult {
-    query_id: String,
-    result_type: String,
-    total_count: usize,
-    result_set: Vec<Entry>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Entry {
-    identifier: String,
-    score: f64,
-}
+use reqwest::{Client};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/search", get(search_handler))
-        .route("/download", get(download_pdb));
+    let app = Router::new().route("/uniprot", get(uniprot_search));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-#[derive(Deserialize)]
+#[derive(Default, Serialize, Deserialize)]
 struct SearchParams {
     search_term: String,
 }
 
-async fn search_handler(Query(params): Query<SearchParams>) -> Result<Json<SearchResult>, StatusCode> {
+// #[derive(Default, Serialize, Deserialize)]
+// struct SearchResults {
+//     primaryAccession: String,
+// }
+
+async fn uniprot_search(Query(params): Query<SearchParams>) -> Result<Json<String>, StatusCode> {
     let search_term = params.search_term;
-    let search_request = serde_json::json!({
-          "query": {
-            "type": "terminal",
-            "service": "full_text",
-            "parameters": {
-              "value": search_term
-            }
-          },
-          "return_type": "entry",
-            "request_options": {
-                "return_all_hits": true
-            }
-        }).to_string();
-    dbg!(&search_request);
+    dbg!(&search_term);
 
+    let api_url = format!("https://rest.uniprot.org/uniprotkb/search?query={}", search_term);
+    dbg!(&api_url);
 
-    let encoded_search_request = encode(&search_request);
-    let api_url = format!("https://search.rcsb.org/rcsbsearch/v2/query?json={}", encoded_search_request);
-    let client = reqwest::Client::new();
-
+    let client: Client = Client::new();
     let search_result_json = client.get(api_url)
         .send()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-        .json::<SearchResult>()
+        .map_err(|e| {
+            dbg!(e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .text()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .map_err(|_| StatusCode::CONFLICT)?;
 
-    Ok(Json(search_result_json))
+
+        // .json::<SearchResults>()
+        // .await
+        // .map_err(|_| StatusCode::CONFLICT)?;
+
+
+    Ok(Json::from(search_result_json))
 }
 
-#[derive(Deserialize)]
-struct DownloadParams {
-    pdb_id: String,
-}
-
-async fn download_pdb(Query(params): Query<DownloadParams>) -> Result<Bytes, StatusCode> {
-    let pdb_id = params.pdb_id;
-
-    let download_url = format!("https://files.rcsb.org/download/{}.pdb", pdb_id);
-
-    let response: Result<Response, StatusCode> = reqwest::get(download_url)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR);
-
-    let pdb_data = response?.bytes().await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR);
-
-    Ok(pdb_data?)
-    // TODO: This will change depending on the frontend process.
-}
-
-struct InfoParams {
-    entry_id: String,
-    entity_id: i8,
-}
-
-#[derive(Deserialize)]
-struct ProteinInfo {
-    rcsb_polymer_entity: String,
-}
-
-async fn get_uniprot_info(Query(params): Query<InfoParams>) -> Result<Json<ProteinInfo>, StatusCode> {
-    let entry_id = params.entry_id;
-    let entity_id = params.entity_id;
-    let search_request = serde_json::json!({
-          "query": {
-            "type": "terminal",
-            "service": "full_text",
-            "parameters": {
-              "value": search_term
-            }
-          },
-          "return_type": "entry",
-            "request_options": {
-                "return_all_hits": true
-            }
-        }).to_string();
-    dbg!(&search_request);
-
-
-    let encoded_search_request = encode(&search_request);
-
-    let api_url = format!("https://rest.uniprot.org/uniprotkb/search?query={}", search_request);
-    let client = reqwest::Client::new();
-
-    let search_result_json = client.get(api_url)
-        .send()
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-        .json::<ProteinInfo>()
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    Ok(Json(search_result_json))
-}
