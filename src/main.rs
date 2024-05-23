ca6c4df8fd1599c3bc0e46b2c48762e5cea71432
Alexander Oroudjev <alexander_oroudjev@waters.com>
2024-05-23 13:33:06 -0700

    Fix uniprotkb response handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use axum::{Json, Router, routing::get};
+use axum_macros::debug_handler;
 use axum::extract::Query;
 use axum::http::StatusCode;
 use serde::{Deserialize, Serialize};
@@ -6,25 +8,27 @@ use serde::{Deserialize, Serialize};
 use reqwest;
 use reqwest::{Client};
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/uniprot", get(uniprot_search));
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct SearchParams {
     search_term: String,
 }
 
-// #[derive(Default, Serialize, Deserialize)]
-// struct SearchResults {
-//     primaryAccession: String,
-// }
+#[derive(Debug, Serialize, Deserialize)]
+struct EntryData {
+    entryType: String,
+    primaryAccession: String,
+    uniProtkbId: String,
+    annotationScore:  f32,
+    organism: HashMap<String, serde_json::Value>,
+}
 
-async fn uniprot_search(Query(params): Query<SearchParams>) -> Result<Json<String>, StatusCode> {
+#[derive(Debug, Serialize, Deserialize)]
+struct SearchResults {
+    results: Vec<EntryData>,
+}
+
+#[debug_handler]
+async fn uniprot_search(Query(params): Query<SearchParams>) -> Result<Json<SearchResults>, StatusCode> {
     let search_term = params.search_term;
     dbg!(&search_term);
 
@@ -32,23 +36,27 @@ async fn uniprot_search(Query(params): Query<SearchParams>) -> Result<Json<Strin
     dbg!(&api_url);
 
     let client: Client = Client::new();
-    let search_result_json = client.get(api_url)
+    let search_result = client.get(api_url)
         .send()
         .await
         .map_err(|e| {
             dbg!(e);
             StatusCode::INTERNAL_SERVER_ERROR
         })?
-        .text()
+        .json::<SearchResults>()
         .await
-        .map_err(|_| StatusCode::CONFLICT)?;
+        .map_err(|e| {
+            dbg!(e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
 
-
-        // .json::<SearchResults>()
-        // .await
-        // .map_err(|_| StatusCode::CONFLICT)?;
-
-
-    Ok(Json::from(search_result_json))
+    Ok(Json(search_result))
 }
 
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(uniprot_search));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
