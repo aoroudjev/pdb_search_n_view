25e1fe7e5754df74e4fbafc869b729eefc61604e
Alexander Oroudjev <alexander_oroudjev@waters.com>
2024-05-24 12:15:33 -0700

    Add alphafold data handler logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use std::collections::HashMap;
 use axum::{Json, Router, routing::get};
 use axum_macros::debug_handler;
+use axum::body::Bytes;
 use axum::extract::Query;
 use axum::http::StatusCode;
 use serde::{Deserialize, Serialize};
 
 use reqwest;
 use reqwest::{Client};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(uniprot_search_handler))
+        .route("/data", get(alphafold_data_handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct SearchParams {
@@ -18,12 +28,11 @@ struct EntryData {
     entryType: String,
     primaryAccession: String,
     uniProtkbId: String,
-    annotationScore:  f32,
+    annotationScore: f32,
     organism: HashMap<String, serde_json::Value>,
-    genes: Vec<HashMap<String, serde_json::Value>>,
     features: Vec<HashMap<String, serde_json::Value>>,
     sequence: serde_json::Value,
-    comments: Vec<HashMap<String, serde_json::Value>>
+    comments: Vec<HashMap<String, serde_json::Value>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -59,23 +68,22 @@ async fn uniprot_search_handler(Query(params): Query<SearchParams>) -> Result<Js
 }
 
 #[debug_handler]
-async fn alphafold_data_handler(Query(params): Query<SearchParams>) -> Result<Json<SearchResults>, StatusCode> {
+async fn alphafold_data_handler(Query(params): Query<SearchParams>) -> Result<Bytes, StatusCode> {
     // Handler for retrieving protein structure data from uniprot for one specific entry
     let search_term = params.search_term;
     dbg!(&search_term);
 
-    let api_url = format!("https://alphafold.ebi.ac.uk/files/{}.pdb", search_term);
+    let api_url = format!("https://alphafold.ebi.ac.uk/files/AF-{}-F1-model_v4.pdb", search_term);
     dbg!(&api_url);
 
+    let client: Client = Client::new();
+    let search_result = client.get(api_url)
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR);
 
-    Ok()
+    let pdb_data = search_result?.bytes().await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
+    Ok(pdb_data)
 }
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(uniprot_search_handler));
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
